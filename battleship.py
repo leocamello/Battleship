5c3d34dc66699cdbf99ca65c61c582221a5ca692
Leonardo Nascimento <leocamello@gmail.com>
2016-02-18 11:22:15 -0200

    ToDo

--- battleship.py
+++ battleship.py
@@ -25,8 +25,6 @@ def random_col(board):
 
 ship_row = random_row(board)
 ship_col = random_col(board)
-guess_row = int(raw_input("Guess Row:"))
-guess_col = int(raw_input("Guess Col:"))
 
 print ship_row
 print ship_col
